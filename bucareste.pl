eb8e8a9f2d0c0d101e2b25cb6c3784cc90793df1
Luis Fernando Rocha de Sousa Alves <luisrsalves@outlook.com>
2017-08-06 18:35:36 -0300

    done

--- bucareste.pl
+++ bucareste.pl
@@ -1,12 +1,87 @@
-﻿vizinhos(arad, [d(sibiu,140), d(timisoara,118), d(zerind,75)]).
+﻿%%%%%%%%%%%%%%%%%%%%%%%%%%%%TESTES%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- use_module(library(plunit)).
+  
+:- begin_tests(calculasomaheuristica).
+test(calculasomaheuristica1) :- calcula_soma_heuristica(rimnicu_vilcea,80, 273).
+test(calculasomaheuristica2) :- calcula_soma_heuristica(urziceni,98, 178).
+test(calculasomaheuristica3) :- calcula_soma_heuristica(timisoara, 111,440).
+test(calculasomaheuristica4) :- calcula_soma_heuristica(craiova,120,280 ).
+test(calculasomaheuristica5) :- calcula_soma_heuristica(mehadia, 70,311).
+test(calculasomaheuristica6) :- calcula_soma_heuristica(arad,140,506).
+:- end_tests(calculasomaheuristica).
+
+:- begin_tests(getmelhorvizinho).
+test(getmelhorvizinho1) :- get_melhor_vizinho([d(mehadia,75), d(craiova,120)], 120, craiova).
+test(getmelhorvizinho2) :- get_melhor_vizinho([d(eforie,86), d(urziceni, 98)],98, urziceni).
+test(getmelhorvizinho3) :- get_melhor_vizinho([d(iasi, 92), d(urziceni, 142)], 142,urziceni).
+test(getmelhorvizinho4) :- get_melhor_vizinho([d(hirsova,86)],86,hirsova ).
+test(getmelhorvizinho5) :- get_melhor_vizinho([d(zerind, 71), d(sibiu, 151)], 151,sibiu).
+test(getmelhorvizinho6) :- get_melhor_vizinho([d(iasi, 92), d(urziceni, 142)],142,urziceni).
+:- end_tests(getmelhorvizinho).
+ 
+:- begin_tests(melhorvizinho).
+test(melhorvizinho1) :- melhor_vizinho(hirsova,urziceni ).
+test(melhorvizinho2) :- melhor_vizinho(urziceni,bucharest ).
+test(melhorvizinho3) :- melhor_vizinho(oradea, sibiu).
+test(melhorvizinho4) :- melhor_vizinho(vaslui,urziceni ).
+test(melhorvizinho5) :- melhor_vizinho(pitesti, bucharest). 
+test(melhorvizinho6) :- melhor_vizinho(neamt,iasi).
+:- end_tests(melhorvizinho).
+
+:- begin_tests(melhorcaminho).
+test(melhorcaminho1) :- melhor_caminho(sibiu, [sibiu, rimnicu_vilcea, pitesti, bucharest]).
+test(melhorcaminho2) :- melhor_caminho(eforie,[eforie, hirsova, urziceni, bucharest] ).
+test(melhorcaminho3) :- melhor_caminho(oradea,[oradea, sibiu, rimnicu_vilcea, pitesti, bucharest] ).
+test(melhorcaminho4) :- melhor_caminho(craiova, [craiova, pitesti, bucharest]).
+test(melhorcaminho5) :- melhor_caminho(pitesti, [pitesti, bucharest]).
+test(melhorcaminho6) :- melhor_caminho(neamt,[neamt, iasi, vaslui, urziceni, bucharest] ).
+:- end_tests(melhorcaminho).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%TESTES%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+vizinhos(arad, [d(sibiu,140),d(timisoara,118), d(zerind,75)]).
 vizinhos(bucharest, [d(fagaras,211), d(giurgiu,90), d(pitesti,101), d(urziceni,85)]).
 vizinhos(craiova, [d(dobreta,120), d(pitesti,138), d(rimnicu_vilcea,146)]).
-% ... continuar
+vizinhos(dobreta, [d(mehadia,75), d(craiova,120)]).
+vizinhos(eforie, [d(hirsova,86)] ).
+vizinhos(fagaras, [d(sibiu,99), d(bucharest,211)] ).
+vizinhos(giurgiu, [d(bucharest,90)]).
+vizinhos(hirsova, [d(eforie,86), d(urziceni, 98)] ).
+vizinhos(iasi, [d(vaslui, 92),d(neamt, 87)]).
+vizinhos(lugoj, [d(mehadia, 70), d(timisoara, 111)]).
+vizinhos(mehadia, [d(dobreta, 75), d(lugoj, 70)]).
+vizinhos(neamt, [d(iasi, 87)]).
+vizinhos(oradea, [d(zerind, 71), d(sibiu, 151)]).
+vizinhos(pitesti, [d(rimnicu_vilcea, 97), d(craiova, 138), d(bucharest, 101)]). 
+vizinhos(rimnicu_vilcea, [d(sibiu, 80), d(craiova, 146), d(pitesti, 97)]).
+vizinhos(sibiu, [d(arad, 140), d(oradea, 151), d(fagaras, 99), d(rimnicu_vilcea, 80)]).
+vizinhos(timisoara, [d(arad, 118), d(lugoj, 111)]).
+vizinhos(urziceni, [d(bucharest, 85), d(hirsova, 98), d(vaslui, 142)]).
+vizinhos(vaslui, [d(iasi, 92), d(urziceni, 142)]).
+vizinhos(zerind, [d(arad, 75), d(oradea, 71)]).
+
 
 heuristica(arad, 366).
 heuristica(bucharest, 0).
 heuristica(craiova, 160).
-% ... continuar
+heuristica(dobreta, 238).
+heuristica(eforie, 161).
+heuristica(fagaras, 178).
+heuristica(giurgiu, 77).
+heuristica(hirsova, 151).
+heuristica(iasi, 226).
+heuristica(lugoj, 244).
+heuristica(mehadia, 241).
+heuristica(neamt, 234).
+heuristica(oradea, 380).
+heuristica(pitesti, 98).
+heuristica(rimnicu_vilcea, 193).
+heuristica(sibiu, 253).
+heuristica(timisoara, 329).
+heuristica(urziceni, 80).
+heuristica(vaslui, 199).
+heuristica(zerind, 374).
+
+
 
 %% melhor_caminho(?O, ?C) is nondet
 %  Verdadeiro se C é o melhor caminho entre O e bucharest
@@ -17,13 +92,70 @@ melhor_caminho(bucharest, [bucharest]) :- !.
 melhor_caminho(O, [O|Cs]) :-
 	melhor_vizinho(O, V), !,
 	melhor_caminho(V, Cs).
-
+ 
 
 %% melhor_vizinho(?O, ?V) is nondet
 %  Verdadeiro se V é o melhor vizinho de O
 %  Exemplo:
 %    ?- melhor_vizinho(sibiu, V).
 %    V = rimnicu_vilcea.
-melhor_vizinho(O, V) :-
-	% ... continuar
-	.
+melhor_vizinho(C, V) :-
+	vizinhos(C,ListaVizinhos),
+    get_melhor_vizinho(ListaVizinhos,_,V),
+	%get_melhor_vizinho(ListaVizinhos,MelhorVizinho,V), caso precise usar o valor [(MelhorVizinho)]
+    !.
+
+
+%% get_melhor_vizinho(ListaVizinhos, MelhorVizinhoAtual,MenorDistancia,MelhorVizinho) 
+%  Verdadeiro se MenorDistancia é a distancia do vizinho, este vizinho é o vizinho cujo sua distancia, 
+%  somada com sua heurística, é a menor dentre os vizinhos
+%  E verdadeiro se MelhorVizinho é o vizinho cujo sua distancia, 
+%  somada com sua heurística, é a menor dentre os vizinhos
+%  Exemplo:
+%    ?- get_melhor_vizinho([d(arad, 140), d(oradea, 151), d(fagaras, 99), d(rimnicu_vilcea, 80)], MelhorVizinhoAtual,MenorDistancia,MelhorVizinho).
+%    MenorDistancia = 80.
+%    MelhorVizinho = rimnicu_vilcea.
+get_melhor_vizinho([Vizinho|ListaVizinhos],MenorValor,MelhorVizinhoFinal):-
+    get_melhor_vizinho(ListaVizinhos,Vizinho,MenorValor,MelhorVizinhoFinal).
+
+get_melhor_vizinho([],MenorVizinho,MenorValor,MelhorVizinhoFinal):-
+    MenorVizinho = d(V,D),
+    MenorValor = D,
+    MelhorVizinhoFinal = V.
+
+get_melhor_vizinho([Vizinho_c|ListaVizinho_r],MelhorVizinho,MenorValor,MelhorVizinhoFinal):-
+    Vizinho_c = d(CidadeVizinho,DistanciaCidade),
+    MelhorVizinho = d(CidadeMelhorVizinho,DistanciaMelhor),
+
+    calcula_soma_heuristica(CidadeVizinho,DistanciaCidade,SomaDistHeuCidade),
+    calcula_soma_heuristica(CidadeMelhorVizinho,DistanciaMelhor,SomaDistHeuMelhor),
+
+    SomaDistHeuMelhor < SomaDistHeuCidade,
+
+    get_melhor_vizinho(ListaVizinho_r, MelhorVizinho, MenorValor,MelhorVizinhoFinal).
+
+   
+get_melhor_vizinho([Vizinho_c|ListaVizinho_r],MelhorVizinho,MenorValor,MelhorVizinhoFinal):-
+    Vizinho_c = d(CidadeVizinho,DistanciaCidade),
+    MelhorVizinho = d(CidadeMelhorVizinho,DistanciaMelhor),
+
+    calcula_soma_heuristica(CidadeVizinho,DistanciaCidade,SomaDistHeuCidade),
+    calcula_soma_heuristica(CidadeMelhorVizinho,DistanciaMelhor,SomaDistHeuMelhor),
+
+    SomaDistHeuMelhor >= SomaDistHeuCidade,
+
+    get_melhor_vizinho(ListaVizinho_r, Vizinho_c, MenorValor,MelhorVizinhoFinal).
+
+
+%% calcula_soma_heuristica(Cidade,Distancia,Resultado) 
+%  Verdadeiro se Resultado é a soma da Distancia da Cidade, com sua heuristica
+%  Exemplo:
+%    ?- calcula_soma_heuristica(rimnicu_vilcea,80, Resultado).
+%    Resultado = 193 + 80.
+
+calcula_soma_heuristica(Cidade,Distancia,Resultado):-
+    heuristica(Cidade,DistanciaHeuristica),
+    Resultado is Distancia + DistanciaHeuristica.
+
+
+
--- rainhas.pl
+++ rainhas.pl
@@ -1,4 +1,115 @@
-﻿%% ------------------- VERSÃO GERAR E TESTAR COM PERMUTAÇÕES -------------------
+﻿%%%%%%%%%%%%%%%%%%%%%%%%%%%%TESTES%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+:- use_module(library(plunit)).
+
+:- begin_tests(sequencia).
+test(sequencia1) :- sequencia(1,1, [1]).
+test(sequencia2) :- sequencia(1,2, [1,2]).
+test(sequencia3) :- sequencia(1,5, [1,2,3,4,5]).
+test(sequencia4) :- sequencia(2,4, [2,3,4]).
+test(sequencia5, [fail]) :- sequencia(2,1, false).
+test(sequencia6, [fail]) :- sequencia(5,1, false).
+:- end_tests(sequencia).
+
+:- begin_tests(permutacao).
+test(permutacao1, [nondet]) :- permutacao([],[]).
+test(permutacao2, [nondet]) :- permutacao([1,2],[1,2]).
+test(permutacao3, [nondet]) :- permutacao([1,2],[2,1]).
+test(permutacao4, [nondet]) :- permutacao([1,2,3],[2,1,3]).
+test(permutacao5, [nondet]) :- permutacao([1,2,3],[3,2,1]).
+test(permutacao6, [fail]) :- permutacao([1,2,3],[1,1,2,3]). 
+:- end_tests(permutacao).
+
+:- begin_tests(entre).
+test(entre1, [nondet]) :- entre(1,3,1).
+test(entre2, [nondet]) :- entre(1,3,2).
+test(entre3, [nondet]) :- entre(1,3,3).
+test(entre4, [nondet]) :- entre(1,5,5).
+test(entre5, [fail]) :- entre(3,1,1).
+test(entre6, [fail]) :- entre(1,3,4).
+:- end_tests(entre).
+
+:- begin_tests(not_contains).
+test(not_contains1) :- not_contains(0,[4, 5, 3, 2, 1]).
+test(not_contains2) :- not_contains(3,[4, 5, 1, 2, 1]).
+test(not_contains3) :- not_contains(8,[4, 5, 3, 2, 1]).
+test(not_contains4, [fail]) :- not_contains(1,[4, 5, 3, 2, 1]).
+test(not_contains5, [fail]) :- not_contains(2,[4, 5, 3, 2, 1]).
+test(not_contains6, [fail]) :- not_contains(3,[4, 5, 3, 2, 1]).
+:- end_tests(not_contains).
+
+%não verifica diagonais, apenas linhas
+
+:- begin_tests(verificaNumIguais).
+test(verificaNumIguais1) :- verificaNumIguais([1, 2, 3, 4, 5]).
+test(verificaNumIguais2) :- verificaNumIguais([2, 3, 5, 7, 9]).
+test(verificaNumIguais3) :- verificaNumIguais([4, 5, 3, 2, 1]).
+test(verificaNumIguais4, [fail]) :- verificaNumIguais([4, 5, 1, 2, 1]).
+test(verificaNumIguais5, [fail]) :- verificaNumIguais([4, 2, 3, 2, 1]).
+test(verificaNumIguais6, [fail]) :- verificaNumIguais([3, 5, 3, 2, 1]).
+:- end_tests(verificaNumIguais).
+
+%não verifica linhas, apenas diagonais
+:- begin_tests(validaDiagonalCrescenteElemento).
+test(validaDiagonalCrescenteElemento1) :- validaDiagonalCrescenteElemento(1,[3, 5, 7, 9]). 
+test(validaDiagonalCrescenteElemento2) :- validaDiagonalCrescenteElemento(3,[5, 2, 4, 8]).
+test(validaDiagonalCrescenteElemento3) :- validaDiagonalCrescenteElemento(2,[4, 1, 6, 8]).
+test(validaDiagonalCrescenteElemento4, [fail]) :- validaDiagonalCrescenteElemento(1,[2, 5, 1, 2, 1]).
+test(validaDiagonalCrescenteElemento5, [fail]) :- validaDiagonalCrescenteElemento(3,[4, 4, 3, 2, 1]).
+test(validaDiagonalCrescenteElemento6, [fail]) :- validaDiagonalCrescenteElemento(2,[3, 5, 3, 2, 1]).
+:- end_tests(validaDiagonalCrescenteElemento).
+
+%não verifica linhas, apenas diagonais
+:- begin_tests(validaDiagonalDecrescenteElemento).
+test(validaDiagonalDecrescenteElemento1) :- validaDiagonalDecrescenteElemento(7,[5, 3, 7, 9]).
+test(validaDiagonalDecrescenteElemento2) :- validaDiagonalDecrescenteElemento(6,[4, 2, 4, 8]).
+test(validaDiagonalDecrescenteElemento3) :- validaDiagonalDecrescenteElemento(5,[3, 1, 6, 8]).
+test(validaDiagonalDecrescenteElemento4, [fail]) :- validaDiagonalDecrescenteElemento(7,[6, 5, 1, 2, 1]).
+test(validaDiagonalDecrescenteElemento5, [fail]) :- validaDiagonalDecrescenteElemento(6,[5, 4, 3, 2, 1]).
+test(validaDiagonalDecrescenteElemento6, [fail]) :- validaDiagonalDecrescenteElemento(5,[4, 5, 3, 2, 1]).
+:- end_tests(validaDiagonalDecrescenteElemento).
+
+:- begin_tests(verificaDiagonais).
+test(verificaDiagonais1) :- verificaDiagonais([2, 4, 6, 8]).
+test(verificaDiagonais2) :- verificaDiagonais([7, 5, 3, 1]).
+test(verificaDiagonais3) :- verificaDiagonais([2, 4, 1, 3]).
+test(verificaDiagonais4, [fail]) :- verificaDiagonais([1 ,2, 4, 3]).
+test(verificaDiagonais5, [fail]) :- verificaDiagonais([3, 2, 4, 1]).
+test(verificaDiagonais6, [fail]) :- verificaDiagonais([2, 4, 5, 8]).
+:- end_tests(verificaDiagonais).
+
+:- begin_tests(rainhas_p).
+test(rainhas_p1, [nondet]) :- rainhas_p([3, 1, 4, 2], 4).
+test(rainhas_p2, [nondet]) :- rainhas_p([2, 4, 1, 3], 4).
+test(rainhas_p3, [nondet]) :- rainhas_p([4, 2, 5, 3, 1], 5).
+test(rainhas_p4, [nondet]) :- rainhas_p([5, 3, 1, 4, 2], 5).
+test(rainhas_p5, [nondet]) :- rainhas_p([1, 4, 2, 5, 3], 5).
+test(rainhas_p6, [nondet]) :- rainhas_p([2, 4, 1, 3, 5], 5).
+test(rainhas_p7, [fail]) :- rainhas_p([1, 2, 3, 4],4).
+test(rainhas_p8, [fail]) :- rainhas_p([1, 4, 3, 2],4).
+test(rainhas_p9, [fail]) :- rainhas_p([2, 4, 5, 8],4).
+test(rainhas_p10, [fail]) :- rainhas_p([1, 4, 1, 3, 5], 5).
+test(rainhas_p11, [fail]) :- rainhas_p([2, 4, 1, 3, 2], 5).
+test(rainhas_p11, [fail]) :- rainhas_p([5, 4, 1, 3, 2], 5).
+:- end_tests(rainhas_p).
+
+
+:- begin_tests(rainhas_n).
+test(rainhas_n1, [nondet]) :- rainhas_n([3, 1, 4, 2], 4).
+test(rainhas_n2, [nondet]) :- rainhas_n([2, 4, 1, 3], 4).
+test(rainhas_n3, [nondet]) :- rainhas_n([4, 2, 5, 3, 1], 5).
+test(rainhas_n4, [nondet]) :- rainhas_n([5, 3, 1, 4, 2], 5).
+test(rainhas_n5, [nondet]) :- rainhas_n([1, 4, 2, 5, 3], 5).
+test(rainhas_n6, [nondet]) :- rainhas_n([2, 4, 1, 3, 5], 5).
+test(rainhas_n7, [fail]) :- rainhas_n([1, 2, 3, 4],4).
+test(rainhas_n8, [fail]) :- rainhas_n([1, 4, 3, 2],4).
+test(rainhas_n9, [fail]) :- rainhas_n([2, 4, 5, 8],4).
+test(rainhas_n10, [fail]) :- rainhas_n([1, 4, 1, 3, 5], 5).
+test(rainhas_n11, [fail]) :- rainhas_n([2, 4, 1, 3, 2], 5).
+test(rainhas_n11, [fail]) :- rainhas_n([5, 4, 1, 3, 2], 5).
+:- end_tests(rainhas_n).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%TESTES%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% ------------------- VERSÃO GERAR E TESTAR COM PERMUTAÇÕES -------------------
 
 %% rainhas_p(?Q, +N) is nondet.
 %  Verdadeiro se Q é uma solução de tamanho N com N rainhas.
@@ -11,16 +122,27 @@ rainhas_p(Q, N) :-
 
 %% sequencia(+I, +F, ?S) is semidet.
 %  Verdadeiro se S é uma lista com os números inteiros entre I e F (inclusive)
-sequencia(I, F, S) :-
-	% ... continuar
-	.
+
+sequencia(X,X,[X]):-!.
+
+sequencia(I,F,[I|Sr]):-
+	I < F,
+	X0 is I + 1,
+	sequencia(X0,F,Sr).
 
 
 %% permutacao(?L, ?P) is nondet.
 %  Verdadeiro se P é uma permutação da lista L
-permutacao(L, P) :-
-	% ... continuar
-	.
+
+permutacao([], []).
+
+permutacao([X], [X]) :-!.
+
+permutacao([Xc|Xr], Y) :- 
+	permutacao(Xr, H1),
+	append(R1, R2, H1),
+	append(R1, [Xc], X1),
+	append(X1, R2, Y).
 
 
 %% -------------------------- VERSÃO COM BACKTRACKING --------------------------
@@ -59,9 +181,13 @@ rainhas_n([], _, 0).
 %    V = 2;
 %    V = 3;
 %    false.
-entre(I, F, V) :-
-	% ... continuar
-	.
+
+entre(X,_,X).
+
+entre(I,F,R):-
+	I < F, 
+	Is is I + 1,
+	entre(Is,F,R).
 
 
 %% solucao(+Q) is semidet.
@@ -71,5 +197,72 @@ entre(I, F, V) :-
 %    ?- solucao([4, 5, 3, 2, 1]).
 %    true.
 solucao(Q) :-
-	% ... continuar
-	.
+	verificaNumIguais(Q),
+	verificaDiagonais(Q).
+
+
+%% not_contains(E,L).
+%  Verdadeiro se E não esta presente em L
+%  Exemplo:
+%    ?- not_contains(0,[4, 5, 3, 2, 1]).
+%    true.
+%    ?- not_contains(1,[4, 5, 3, 2, 1]).
+%    false.
+not_contains(_,[]):-!.
+not_contains(E,[Lc|Lr]):-
+	E \= Lc,
+	not_contains(E,Lr).
+
+
+%% verificaNumIguais(L).
+%  Verdadeiro se L não apresenta elementos repetidos
+%  Exemplo:
+%    ?- verificaNumIguais([4, 5, 3, 2, 1]).
+%    true.
+%    ?- verificaNumIguais([4, 2, 3, 2, 1]).
+%    false.
+verificaNumIguais([]).
+
+verificaNumIguais([Lc|Lr]):-
+	not_contains(Lc,Lr), 
+	verificaNumIguais(Lr).
+
+
+%% validaDiagonalCrescenteElemento(E,L).
+%  Verdadeiro se L não apresenta elementos em suas diagonais Crescentes
+%  Exemplo:
+%    ?- validaDiagonalCrescenteElemento(1,[3,...]).
+%    true.
+%    ?- validaDiagonalCrescenteElemento(1,[2,...]).
+%    false.
+validaDiagonalCrescenteElemento(_,[]):-!.
+validaDiagonalCrescenteElemento(E,[Lc|Lr]):- 
+	E0 is E + 1,
+	E0 \= Lc,
+	validaDiagonalCrescenteElemento(E0,Lr).
+
+%% validaDiagonalDecrescenteElemento(E,L).
+%  Verdadeiro se L não apresenta elementos em suas diagonais Descrescentes
+%  Exemplo:
+%    ?- validaDiagonalDecrescenteElemento(3,[5,...]).
+%    true.
+%    ?- validaDiagonalDecrescenteElemento(3,[2,...]).
+%    false.
+validaDiagonalDecrescenteElemento(_,[]):-!.
+validaDiagonalDecrescenteElemento(E,[Lc|Lr]):-
+	E0 is E - 1,
+	E0 \= Lc,
+	validaDiagonalDecrescenteElemento(E0,Lr).
+
+%% verificaDiagonais(L).
+%  Verdadeiro se L não apresenta elementos em suas diagonais Crescentese Descrescentes
+%  Exemplo:
+%    ?- verificaDiagonais([2,4,1,3...]).
+%    true.
+%    ?- verificaDiagonais([2,3,5,4,...]).
+%    false.
+verificaDiagonais([]).
+verificaDiagonais([Lc|Lr]):-
+	validaDiagonalCrescenteElemento(Lc,Lr),
+	validaDiagonalDecrescenteElemento(Lc,Lr),
+	verificaDiagonais(Lr).
